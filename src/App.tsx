a441f48e3956c2532a5fe89ba685d3e844b2be90
조하은 <jhe93@naver.com>
2025-09-10 21:25:20 +0900

    chore(#6): Biome 코드 포맷터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 function App() {
-    return (
-        <>
-            <div className="bg-bronze font-pretendard">브론즈</div>
-            <div className="bg-silver font-pretendard">실버</div>
-            <div className="bg-gold font-mbc">골드</div>
-            <div className="bg-platinum">플래티넘</div>
-            <div className="bg-diamond">다이아몬드</div>
-        </>
-    );
+	return (
+		<>
+			<div className="bg-bronze font-pretendard">브론즈</div>
+			<div className="bg-silver font-pretendard">실버</div>
+			<div className="bg-gold font-mbc">골드</div>
+			<div className="bg-platinum">플래티넘</div>
+			<div className="bg-diamond">다이아몬드</div>
+		</>
+	);
 }
 
 export default App;
