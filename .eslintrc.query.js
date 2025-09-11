b9104efa991bdb94487cce3401343fba2a11c1dc
조하은 <jhe93@naver.com>
2025-09-11 18:45:34 +0900

    fix: biome 스크립트 수정
    - `TanStack Query plugin`을 일시적으로 제거. 추후 안정적인 설정 방법을 찾아서 재적용 예정.

--- .eslintrc.query.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['@tanstack/eslint-plugin-query/recommended'],
-  rules: {
-    '@tanstack/query/exhaustive-deps': 'error', // 쿼리 의존성 검사
-    '@tanstack/query/stable-query-client': 'error' // 쿼리 클라이언트 안정성
-  }
-}
