f42c8664f6d81921ae87836a0f90d1aa7435f190
조하은 <jhe93@naver.com>
2025-09-12 16:20:18 +0900

    chore(#7): alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 
 export default defineConfig({
     plugins: [tanstackRouter({
+      routesDirectory: "./src/pages",
+      generatedRouteTree: "./src/app/@generated/routeTree.gen.ts",
       target: 'react',
       autoCodeSplitting: true,
     }),react(), tailwindcss(),],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    }
 });
