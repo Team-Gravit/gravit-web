95d43afd7dd3ba1a254f542c6a2a5602666b96c1
조하은 <jhe93@naver.com>
2025-09-19 00:10:19 +0900

    feat(#25): 행성 관련 타입/유틸함수 작성

---
+++ src/shared/assets/images/planets/index.ts
@@ -0,0 +1,21 @@
+import MercuryImg from "./Mercury.png";
+import VenusImg from "./Venus.png";
+import EarthImg from "./Earth.png";
+import MarsImg from "./Mars.png";
+import JupiterImg from "./Jupiter.png";
+import SaturnImg from "./Saturn.png";
+import UranusImg from "./Uranus.png";
+import NeptuneImg from "./Neptune.png";
+import MoonImg from "./Moon.png";
+
+export const PLANET_IMAGES = {
+	1: MercuryImg,
+	2: VenusImg,
+	3: EarthImg,
+	4: MarsImg,
+	5: JupiterImg,
+	6: SaturnImg,
+	7: UranusImg,
+	8: NeptuneImg,
+	9: MoonImg,
+} as const;
---
+++ src/shared/lib/planet/types.ts
@@ -0,0 +1,3 @@
+import type { PLANET_IMAGES } from "@/shared/assets/images/planets";
+
+export type PlanetId = keyof typeof PLANET_IMAGES;
