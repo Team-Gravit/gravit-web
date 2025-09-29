125a46f8d3c9f102e40e500318aa1e072e495ece
조하은 <jhe93@naver.com>
2025-09-30 01:43:04 +0900

    add: api 응답 타입 추가

--- src/shared/api/types.ts
+++ src/shared/api/types.ts
@@ -5,3 +5,12 @@ export interface Chapter {
 	totalUnits: number;
 	completedUnits: number;
 }
+
+export type ApiSuccessResponse<T> = T;
+
+export interface ApiErrorResponse {
+	error: string;
+	message: string;
+}
+
+export type ApiResponse<T> = ApiSuccessResponse<T> | ApiErrorResponse;
