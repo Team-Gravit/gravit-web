7f9ab42d6175e89f7e70ea17d3b9708dc1a453e4
hyewon <helena030828@naver.com>
2025-10-10 01:40:15 +0900

    온보딩 성공 페이지 디자인 수정

--- src/pages/_authenticated/_onboarding/success.tsx
+++ src/pages/_authenticated/_onboarding/success.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import Form from "@/shared/ui/form/Form";
 import Logo from "@/shared/ui/logo/Logo";
 import { useNavigate } from "@tanstack/react-router";
-import Greeting from "@/shared/assets/icons/greeting.svg?react";
+import Mascot from "@/shared/assets/icons/end-mascot.svg?react";
 
 export const Route = createFileRoute("/_authenticated/_onboarding/success")({
 	component: SuccessPage,
@@ -15,15 +15,15 @@ function SuccessPage() {
 		<>
 			<Logo />
 			<Form className="w-[549px] h-[460px] py-8 px-28">
-				<h3 className="text-2xl font-semibold">계정 생성 완료!</h3>
-				<span className="text-[16px] text-[#4C4C4C] mt-4">
+				<h3 className="text-2xl font-semibold text-white">계정 생성 완료!</h3>
+				<span className="text-[16px] text-[#DCDCDC] mt-4 mb-4">
 					그래빗의 일원이 된 걸 환영해요!
 				</span>
-				<Greeting />
+				<Mascot className="h-56" />
 				<button
 					type="button"
 					onClick={() => navigate({ to: "/main" })}
-					className="mt-auto w-full h-14 text-white py-2 rounded-xl text-lg font-semibold bg-[#8100B3]"
+					className="mt-auto w-full h-14 text-white py-2 rounded-xl text-lg font-semibold bg-[#BA00FF]"
 				>
 					홈으로
 				</button>
