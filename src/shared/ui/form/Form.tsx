28124b1eca353d3eb411dcc0050f8aa32807accb
hyewon <helena030828@naver.com>
2025-10-10 01:27:30 +0900

    폼 디자인 수정

--- src/shared/ui/form/Form.tsx
+++ src/shared/ui/form/Form.tsx
@@ -8,7 +8,17 @@ interface FormProps {
 export default function Form({ children, className }: FormProps) {
 	return (
 		<section
-			className={`flex flex-col rounded-2xl bg-white items-center ${className ?? ""}`}
+			className={`
+        box-border flex flex-col items-center 
+        rounded-xl border-2 border-[#8B69FF]
+        shadow-[0_0_50px_-15px_rgba(0,0,0,0.5)]
+        [backdrop-filter:blur(50px)]
+        ${className ?? ""}
+      `}
+			style={{
+				background:
+					"linear-gradient(108.74deg, rgba(255,255,255,0.24) 0%, rgba(255,255,255,0.06) 100%)",
+			}}
 		>
 			{children}
 		</section>
