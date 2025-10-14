23acb2ebfea967a0e64e9e6a1ea6b2a6ee5ee56c
조하은 <jhe93@naver.com>
2025-10-15 01:47:56 +0900

    fix: 미션 카드 수정

--- src/widgets/misson-widget/MissionCard.tsx
+++ src/widgets/misson-widget/MissionCard.tsx
@@ -1,10 +1,13 @@
 import StartMissionButton from "@/features/mission/StartMissionButton";
 import Card from "@/shared/ui/card/Card";
+import CompletedStampIcon from "@/shared/assets/icons/misson-completed.svg?react";
 
 export default function MissionCard({
 	missionInfo,
+	isCompleted,
 }: {
 	missionInfo: { missionName: string; awardXp: number };
+	isCompleted: boolean;
 }) {
 	return (
 		<Card className=" h-full w-2/3 min-h-[334px] flex flex-col justify-between p-4 ">
@@ -13,16 +16,22 @@ export default function MissionCard({
 					오늘의 미션🔥
 				</h3>
 				<hr className="text-gray-500 border-dashed border-2" />
-				<ul className="flex list-disc pl-5 leading-none">
-					<li className="text-2xl font-medium">
-						{missionInfo.missionName}
-						<small className="block text-xl text-gray-800 font-normal mt-1">
-							완료 시 {missionInfo.awardXp}xp
-						</small>
-					</li>
-				</ul>
+				{isCompleted ? (
+					<div className="w-full flex-grow flex items-center justify-center mt-2">
+						<CompletedStampIcon />
+					</div>
+				) : (
+					<ul className="flex list-disc pl-5 leading-none">
+						<li className="text-2xl font-medium">
+							{missionInfo.missionName}
+							<small className="block text-xl text-gray-800 font-normal mt-1">
+								완료 시 {missionInfo.awardXp}xp
+							</small>
+						</li>
+					</ul>
+				)}
 			</div>
-			<StartMissionButton />
+			<StartMissionButton isCompleted={isCompleted} />
 		</Card>
 	);
 }
