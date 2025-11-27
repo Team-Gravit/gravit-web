5eedb0893d2a6793e02c14a7fa1573b23afe4e08
조하은 <jhe93@naver.com>
2025-11-28 02:37:08 +0900

    refactor: 인터셉터 제외 요청을 위한 authClient 분리 및 인스턴스 생성 구조화

---
+++ src/shared/api/index.ts
@@ -0,0 +1,84 @@
+import { Configuration } from "./@generated";
+import type { AxiosInstance } from "axios";
+import {
+	AuthTokenAPIApi,
+	BadgeApi,
+	CSNoteAPIApi,
+	FriendAPIApi,
+	FriendSearchAPIApi,
+	LeagueAPIApi,
+	LeagueRankAPIApi,
+	LearningAPIApi,
+	MyLeagueProfileAPIApi,
+	NoticeQueryAPIApi,
+	OAuth20APIApi,
+	UserAPIApi,
+	UserDeletionAPIApi,
+} from "./@generated";
+import { apiClient, authClient, privateApiConfiguration } from "./config";
+
+function createApiInstance<T>(
+	ApiClass: new (
+		config?: Configuration,
+		basePath?: string,
+		axiosInstance?: AxiosInstance,
+	) => T,
+	config: Configuration,
+	axiosInstance: AxiosInstance,
+): T {
+	return new ApiClass(config, undefined, axiosInstance);
+}
+
+export const api = {
+	auth: {
+		oAuth: createApiInstance(
+			OAuth20APIApi,
+			privateApiConfiguration,
+			authClient,
+		),
+		refresh: createApiInstance(
+			AuthTokenAPIApi,
+			privateApiConfiguration,
+			authClient,
+		),
+	},
+	note: createApiInstance(CSNoteAPIApi, privateApiConfiguration, apiClient),
+	league: createApiInstance(LeagueAPIApi, privateApiConfiguration, apiClient),
+	ranking: {
+		user: createApiInstance(
+			MyLeagueProfileAPIApi,
+			privateApiConfiguration,
+			apiClient,
+		),
+		global: createApiInstance(
+			LeagueRankAPIApi,
+			privateApiConfiguration,
+			apiClient,
+		),
+	},
+	user: {
+		delete: createApiInstance(
+			UserDeletionAPIApi,
+			privateApiConfiguration,
+			apiClient,
+		),
+		manage: createApiInstance(UserAPIApi, privateApiConfiguration, apiClient),
+	},
+	friend: createApiInstance(FriendAPIApi, privateApiConfiguration, apiClient),
+	friendSearch: createApiInstance(
+		FriendSearchAPIApi,
+		privateApiConfiguration,
+		apiClient,
+	),
+	learning: createApiInstance(
+		LearningAPIApi,
+		privateApiConfiguration,
+		apiClient,
+	),
+	badge: createApiInstance(BadgeApi, privateApiConfiguration, apiClient),
+	notice: createApiInstance(
+		NoticeQueryAPIApi,
+		privateApiConfiguration,
+		apiClient,
+	),
+};
