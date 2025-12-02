da5bf968c5944a984edc4c8bca8bfa2f7e16908e
hyewon <helena030828@naver.com>
2025-12-02 19:48:51 +0900

    수정사항 반영

--- src/pages/_authenticated/_fixed-header-layout/learning/$chapterId/$unitId/index.tsx
+++ src/pages/_authenticated/_fixed-header-layout/learning/$chapterId/$unitId/index.tsx
@@ -5,7 +5,6 @@ import ChapterInfo from "@/entities/chapter/ChapterInfo";
 import NextIcon from "@/shared/assets/icons/ic-right-arrow.svg?react";
 import BookmarkIcon from "@/shared/assets/icons/ic-bookmark.svg?react";
 import { useFetchLessons } from "@/entities/learning/model/hooks";
-import { useNavigate } from "@tanstack/react-router";
 
 export const Route = createFileRoute(
 	"/_authenticated/_fixed-header-layout/learning/$chapterId/$unitId/",
@@ -14,7 +13,6 @@ export const Route = createFileRoute(
 });
 
 function RouteComponent() {
-	const navigate = useNavigate();
 	const { chapterId, unitId } = Route.useParams();
 	const { lessons, chapterInfo, isPending } = useFetchLessons(Number(unitId));
 
@@ -34,8 +32,10 @@ function RouteComponent() {
 						chapterName={chapterInfo.chapterName}
 						chapterInfoText={chapterInfo.chapterDescription}
 					/>
-					<button
-						type="button"
+					<Link
+						to={"/learning/$chapterId/$unitId/concept-note"}
+						params={{ chapterId, unitId }}
+						search={{ chapterName: chapterInfo.chapterName }}
 						className="
 								flex justify-center items-center
 								w-[180px] h-[76px]
@@ -45,13 +45,9 @@ function RouteComponent() {
 								text-white font-bold text-[30px] leading-[36px]
 								hover:brightness-110 transition
 								"
-						onClick={() => {
-							// 개념노트 페이지로 이동
-							navigate({ to: `/learning/${chapterId}/${unitId}/concept-note` });
-						}}
 					>
 						개념노트
-					</button>
+					</Link>
 				</div>
 				<div className="w-full grid grid-rows-[auto_auto] grid-cols-[1fr_1fr] gap-x-8 gap-y-7 lg:gap-x-16 lg:gap-y-14">
 					<section className="flex flex-col gap-4">
