efdc4009c3707370b5f8a209ce60359b12135d51
조하은 <jhe93@naver.com>
2025-12-02 19:02:25 +0900

    feat: Confirm 모달 추가

--- src/widgets/learning-widget/ui/LessonQuizComponent.tsx
+++ src/widgets/learning-widget/ui/LessonQuizComponent.tsx
@@ -2,14 +2,18 @@ import { useFetchProblems } from "@/entities/learning/model/hooks";
 import LoadingWidget from "@/widgets/learning-widget/LoadingWidget";
 import { useMinimumLoadingTime } from "@/widgets/learning-widget/lib/useMinimumLoadingTime";
 import { useEffect, useRef } from "react";
-import { type ConfirmModalRef } from "@/shared/ui/modal/ConfirmModal";
 import QuizHeader from "@/widgets/learning-widget/QuizHeader";
 import QuizProgressBar from "@/widgets/learning-widget/QuizProgressBar";
 import ProblemStatement from "@/entities/learning/ui/ProblemStatement";
 import AnswerInteraction from "@/widgets/learning-widget/AnswerInteraction";
 import { useQuizSessionState } from "@/features/quiz/model/quiz-session-store";
 import QuizResultWidget from "../QuizResultWidget";
-// import QuizResultWidget from "../QuizResultWidget";
+import {
+	ConfirmModal,
+	type ConfirmModalRef,
+} from "@/shared/ui/modal/ConfirmModal";
+import ReportButton from "@/features/quiz/submit-report/ui/ReportButton";
+import { useRouter } from "@tanstack/react-router";
 
 export default function LessonQuizComponent({
 	lessonId,
@@ -17,6 +21,7 @@ export default function LessonQuizComponent({
 	lessonId: string;
 }) {
 	const { data, isPending } = useFetchProblems(Number(lessonId));
+	const router = useRouter();
 
 	const quitModalRef = useRef<ConfirmModalRef>(null);
 
@@ -28,6 +33,7 @@ export default function LessonQuizComponent({
 	const resetTime = useQuizSessionState((state) => state.resetTime);
 	const isQuizCompleted = useQuizSessionState((state) => state.isQuizCompleted);
 	const pauseTime = useQuizSessionState((state) => state.pauseTime);
+	const resumeTime = useQuizSessionState((state) => state.resumeTime);
 
 	//학습 결과 제출 상태
 	const isSubmittingResult = useQuizSessionState(
@@ -56,7 +62,7 @@ export default function LessonQuizComponent({
 		return <div>문제가 발생했습니다.</div>;
 	}
 
-	const { problems, totalProblems } = data;
+	const { problems, totalProblems, unitSummary } = data;
 	const percent = (userAnswers.length / problems.length) * 100;
 
 	const currentProblem = problems[currentProblemIndex];
@@ -72,13 +78,29 @@ export default function LessonQuizComponent({
 		<>
 			{!isQuizCompleted && (
 				<div className="w-full h-screen flex flex-col">
+					<ConfirmModal
+						ref={quitModalRef}
+						title={["지금까지 푼 내역이", "모두 사라져요!"]}
+						description={["학습이 중단됩니다", "정말 학습을 그만두시나요?"]}
+						confirmText="그만두기"
+						cancelText="계속하기"
+						onConfirm={() => {
+							resetQuiz();
+							resetTime();
+							router.history.back();
+						}}
+						onCancel={() => {
+							resumeTime();
+						}}
+					/>
+
 					<QuizHeader
-						learningTitle={data.unitSummary.title}
+						learningTitle={unitSummary.title}
 						onHandleQuit={handleClickQuit}
 					/>
 					<QuizProgressBar progress={`${percent}%`} />
 					<main className=" bg-gray-200 flex flex-col items-center h-full">
-						<div className="flex flex-col gap-15 w-full h-full max-w-[1500px] 3xl:w-[80%] pt-15 px-10 lg:px-20">
+						<div className="flex flex-col gap-15 w-full h-full max-w-[1500px] 3xl:w-[80%] pt-15 px-10 lg:px-20 relative">
 							<ProblemStatement
 								problem={currentProblem}
 								totalProblemsCount={totalProblems}
