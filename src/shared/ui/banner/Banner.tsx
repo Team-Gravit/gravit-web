6cec9ceb08a318df71699145ffe2fbea261321b9
조하은 <jhe93@naver.com>
2025-12-08 23:00:08 +0900

    fix: 배너 컴포넌트 수정

--- src/shared/ui/banner/Banner.tsx
+++ src/shared/ui/banner/Banner.tsx
@@ -1,4 +1,4 @@
-// import Logo from "@/shared/assets/icons/logo.svg?react";
+import Logo from "@/shared/assets/icons/logo.svg?react";
 import bannerImage from "@/shared/assets/images/banner.webp";
 
 export default function Banner() {
@@ -10,12 +10,12 @@ export default function Banner() {
 			}}
 		>
 			<div className="w-full h-full relative pt-[76px]">
-				{/* <div className="absolute flex flex-col items-start justify-center z-10 text-white left-[147px] top-1/2 transform -translate-y-1/2 gap-4 pt-[76px]">
+				<div className="absolute flex flex-col items-start justify-center z-10 text-white left-[147px] top-1/2 transform -translate-y-1/2 gap-4">
 					<Logo className="w-[370px] brightness-0 invert" />
 					<p className="font-bold text-xl text-white">
 						그래빗과 함께 CS 지식을 마스터해요!
 					</p>
-				</div> */}
+				</div>
 			</div>
 		</section>
 	);
