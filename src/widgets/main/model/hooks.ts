0c90ecfe21734cbf81517e626e51361d870f9c15
조하은 <jhe93@naver.com>
2026-01-06 23:08:39 +0900

    fix: 레슨 후 메인 화면의 최근 진행 학습도 업데이트 되도록 수정

--- src/widgets/main/model/hooks.ts
+++ src/widgets/main/model/hooks.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { api } from "@/shared/api";
+import { learningKeys } from "@/entities/learning/api/query-keys";
 
 export const useFetchMainInfo = () => {
 	const query = useQuery({
-		queryKey: ["main-info"],
+		queryKey: ["main-info", learningKeys.all],
 		queryFn: () => api.user.manage.getMainPage(),
 	});
 
