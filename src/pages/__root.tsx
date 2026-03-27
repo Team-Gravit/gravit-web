71fb40a6354c9900e598b147cf21509ddbba4c51
조하은 <jhe93@naver.com>
2026-03-27 20:57:58 +0900

    fix: 서버 점검중 문구 제거

--- src/pages/__root.tsx
+++ src/pages/__root.tsx
@@ -1,11 +1,9 @@
-import { createRootRouteWithContext } from "@tanstack/react-router";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { useEffect, useState } from "react";
 import { isMobileDevice } from "@/shared/lib/detect-mobile";
-// import MobileAppInstallGuide from "@/widgets/mobile-block/MobileAppInstallGuide";
+import MobileAppInstallGuide from "@/widgets/mobile-block/MobileAppInstallGuide";
 import Page404Component from "@/widgets/error-widget/Page404Component";
-import ErrorPageTemplate from "@/widgets/error-widget/ErrorPageTemplate";
-import Mascot404Icon from "@/shared/assets/icons/ic-mascot-error.svg?react";
 
 export interface RouteContext {
 	auth: AuthState;
@@ -29,31 +27,14 @@ const RootLayout = () => {
 	}
 
 	// 모바일이면 앱 설치 안내 화면만 표시
-	// if (isMobile) {
-	// 	return <MobileAppInstallGuide />;
-	// }
+	if (isMobile) {
+		return <MobileAppInstallGuide />;
+	}
 
 	// PC면 정상적으로 라우터 사용
 	return (
 		<>
-			{/* <Outlet /> */}
-			<ErrorPageTemplate
-				icon={<Mascot404Icon className="w-[300px]" />}
-				title="점검중입니다."
-				description={
-					<>
-						불편을 끼쳐드려 죄송합니다.
-						<br />
-						서버 점검으로 인해
-						<br />
-						일시적으로 접속이 불가하오니
-						<br />
-						많은 양해 부탁드립니다.
-					</>
-				}
-			>
-				<div></div>
-			</ErrorPageTemplate>
+			<Outlet />
 			<TanStackRouterDevtools />
 		</>
 	);
