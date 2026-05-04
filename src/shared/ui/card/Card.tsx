a329b82e1cc5d3264f7fa3e4e673647763eff721
조하은 <jhe93@naver.com>
2026-05-04 23:28:15 +0900

    feat: Card 컴포넌트 구현

--- src/shared/ui/card/Card.tsx
+++ src/shared/ui/card/Card.tsx
@@ -1,18 +1,80 @@
-import type { HTMLAttributes, ReactNode } from "react";
 import { cn } from "@/shared/lib/cn";
+import { cva, type VariantProps } from "class-variance-authority";
 
-interface CardProps extends HTMLAttributes<HTMLDivElement> {
-	children: ReactNode;
+const cardVariants = cva("box-border flex flex-col bg-white w-full", {
+	variants: {
+		size: {
+			sm: "p-3 rounded-lg gap-2",
+			md: "p-5 rounded-xl gap-4", // 현재 나온 디자인은 md 사이즈만 존재하지만, 추후 사이즈가 추가될 수 있으므로 다른 사이즈도 정의해둠
+			lg: "p-7 rounded-2xl gap-6",
+		},
+	},
+	defaultVariants: {
+		size: "md",
+	},
+});
+
+interface CardProps extends VariantProps<typeof cardVariants> {
+	children?: React.ReactNode;
 	className?: string;
 }
 
-export default function Card({ children, className, ...props }: CardProps) {
+function CardHeader({
+	children,
+	className,
+}: {
+	children: React.ReactNode;
+	className?: string;
+}) {
 	return (
-		<div
-			className={cn("bg-white rounded-2xl custom-shadow", className)}
-			{...props}
-		>
+		<div className={cn("w-full flex items-center justify-between", className)}>
 			{children}
 		</div>
 	);
 }
+
+function CardTitle({
+	children,
+	className,
+}: {
+	children: React.ReactNode;
+	className?: string;
+}) {
+	return (
+		<h3 className={cn("font-medium text-base text-gray-500", className)}>
+			{children}
+		</h3>
+	);
+}
+
+function CardLink({
+	url,
+	children,
+	className,
+}: {
+	url: string;
+	children: React.ReactNode;
+	className?: string;
+}) {
+	return (
+		<a
+			href={url}
+			className={cn(
+				"font-medium text-base text-gray-400 underline underline-offset-2 shadow-[0px_4px_32px_0px_#00000006]",
+				className,
+			)}
+		>
+			{children}
+		</a>
+	);
+}
+
+export default function Card({ size = "md", children, className }: CardProps) {
+	return (
+		<div className={cn(cardVariants({ size }), className)}>{children}</div>
+	);
+}
+
+Card.Header = CardHeader;
+Card.Title = CardTitle;
+Card.Link = CardLink;
