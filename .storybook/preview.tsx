f5c61cbabaf28aa30609a5abaf6b85cc1d01176d
조하은 <jhe93@naver.com>
2026-05-13 13:09:27 +0900

    feat: 컬러 토큰 추가 및 css 파일 분리

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
-import '../src/app/styles/globals.css'
+import '../src/app/styles/index.css'
 import type { Preview } from '@storybook/react-vite'
 import { withTanstackRouter } from '../src/shared/lib/test/with-router';
 import { MINIMAL_VIEWPORTS } from "storybook/viewport";
