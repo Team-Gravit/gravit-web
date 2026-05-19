098da02c951179eb5e7a7b6a3212b78471a70843
Ajungmiiin <wjdals3249@naver.com>
2026-05-19 17:45:14 +0900

    design : 우측 상단 설정 요소 수정

--- src/widgets/user/ui/user-profile-card.tsx
+++ src/widgets/user/ui/user-profile-card.tsx
@@ -37,54 +37,57 @@ export default function UserProfileCard({
 				backgroundSize: "cover",
 				backgroundPosition: "center",
 			}}
-			className="relative h-[196px] md:h-[204px] w-full max-w-300 md:rounded-xl overflow-hidden"
+			className="relative min-h-[196px] md:min-h-[204px] w-full  md:rounded-xl overflow-hidden"
 		>
 			<div className="absolute inset-0 size-full bg-black/40 pointer-events-none md:bg-linear-to-l md:to-[#1D0027]/0 md:via-transparent md:from-0% md:via-15% md:from-[#1D0027]/60" />
-			<div className="absolute inset-0 size-full px-4 py-6 md:p-8">
-				<div className="flex items-center gap-3 absolute top-6 right-4 md:top-8 md:right-8">
-					<Button
-						type="button"
-						onClick={handleEditProfileClick}
-						className="max-md:hidden"
-					>
-						프로필 수정
-					</Button>
+			<div className="relative flex flex-col w-full h-full gap-3 px-4 py-6 md:p-8">
+				<div className="flex-1 flex min-h-0">
+					<div className="flex-1 flex items-center gap-3 md:gap-8">
+						<div>
+							<Profile
+								style={{ color: getProfileColor(profileImageNumber) }}
+								className="size-[70px] md:size-[140px]"
+							/>
+						</div>
 
-					<Link
-						aria-label="사용자 설정 페이지로 이동"
-						className="flex items-center justify-center"
-						to="/user"
-					>
-						<SettingIcon />
-					</Link>
-				</div>
-				<div className="flex items-center gap-3 md:gap-8 mb-2">
-					<Profile
-						style={{ color: getProfileColor(profileImageNumber) }}
-						className="size-[70px] md:size-[140px]"
-					/>
+						<div className="flex flex-col gap-1">
+							<h3 className="heading2 text-text-1-w md:display2">{nickname}</h3>
 
-					<div className="flex flex-col gap-1">
-						<h3 className="heading2 text-text-1-w md:display2">{nickname}</h3>
+							<p className="text-semantic-info body1-normal mb-2 max-md:hidden">
+								{`@${handle}`}
+							</p>
 
-						<p className="text-semantic-info body1-normal mb-2 max-md:hidden">
-							{`@${handle}`}
-						</p>
-
-						<div className="flex items-center gap-1 md:gap-2 text-cta-text caption1 md:headline2">
-							<ProfileCardLabel>{`LV. ${level}`}</ProfileCardLabel>
-							<ProfileCardLabel>{currentLeague}</ProfileCardLabel>
-							<ProfileCardLabel className="max-md:hidden">
-								{`${consecutiveSolvedDays}일 연속 학습중`}
-							</ProfileCardLabel>
+							<div className="flex items-center gap-1 md:gap-2 text-cta-text caption1 md:headline2">
+								<ProfileCardLabel>{`LV. ${level}`}</ProfileCardLabel>
+								<ProfileCardLabel>{currentLeague}</ProfileCardLabel>
+								<ProfileCardLabel className="max-md:hidden">
+									{`${consecutiveSolvedDays}일 연속 학습중`}
+								</ProfileCardLabel>
+							</div>
 						</div>
 					</div>
-				</div>
 
-				<p className="text-semantic-info body2-normal text-sm mb-3 md:hidden">
+					<div className="flex items-center gap-3 self-start">
+						<Button
+							type="button"
+							onClick={handleEditProfileClick}
+							className="max-md:hidden"
+						>
+							프로필 수정
+						</Button>
+
+						<Link
+							aria-label="사용자 설정 페이지로 이동"
+							className="flex items-center justify-center"
+							to="/user"
+						>
+							<SettingIcon />
+						</Link>
+					</div>
+				</div>
+				<p className="text-semantic-info body2-normal text-sm md:hidden">
 					{`@${handle}`}
 				</p>
-
 				<Button
 					type="button"
 					variant={"secondary"}
