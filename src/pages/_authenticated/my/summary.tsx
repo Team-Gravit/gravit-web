ceffeee71624f248dce1285db49b0a7544793ed8
조하은 <jhe93@naver.com>
2026-05-27 20:38:42 +0900

    feat: summary 페이지 구현

--- src/pages/_authenticated/my/summary.tsx
+++ src/pages/_authenticated/my/summary.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
+import SummaryCard from "@/widgets/my-page/summary/summary-card";
+import StudyHeatmap from "@/widgets/my-page/summary/study-heatmap";
 
 export const Route = createFileRoute("/_authenticated/my/summary")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Hello "/_authenticated/my/summary"!</div>;
+	return (
+		<div className="flex flex-col gap-3 md:gap-6">
+			<SummaryCard />
+			<StudyHeatmap />
+		</div>
+	);
 }
