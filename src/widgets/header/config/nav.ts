ef43917b9ffd03d308aad1a77eb1d93528fe2ff5
Ajungmiiin <wjdals3249@naver.com>
2026-05-31 03:49:25 +0900

    fix : 마이 페이지 경로 수정

--- src/widgets/header/config/nav.ts
+++ src/widgets/header/config/nav.ts
@@ -2,5 +2,5 @@ export const DEFAULT_HEADER_NAV_LIST = [
 	{ to: "/main", label: "홈" },
 	{ to: "/learning", label: "학습" },
 	{ to: "/league", label: "리그" },
-	{ to: "/user", label: "마이그래빗" },
+	{ to: "/my", label: "마이그래빗" },
 ];
