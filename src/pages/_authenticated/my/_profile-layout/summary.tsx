4f54d2d0ac63dc1d7b728866535419ee62b6f4d5
조하은 <jhe93@naver.com>
2026-06-03 16:39:27 +0900

    feat: 마이페이지 요약 탭 API 연동

--- src/pages/_authenticated/my/_profile-layout/summary.tsx
+++ src/pages/_authenticated/my/_profile-layout/summary.tsx
@@ -1,16 +1,22 @@
 import { createFileRoute } from "@tanstack/react-router";
 import SummaryCard from "@/widgets/my-page/summary/summary-card";
 import StudyHeatmap from "@/widgets/my-page/summary/study-heatmap";
+import { useGetMyPageSummary } from "@/shared/api/@generated/mypage-api/mypage-api";
 
 export const Route = createFileRoute("/_authenticated/my/_profile-layout/summary")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
+
+	const {data, isPending, isError} = useGetMyPageSummary();
+
+	if(isPending || isError) return;
+
 	return (
 		<div className="flex flex-col gap-3 md:gap-6">
-			<SummaryCard />
-			<StudyHeatmap />
+			<SummaryCard learningSummary={data.learningSummary}/>
+			<StudyHeatmap/>
 		</div>
 	)
 }
