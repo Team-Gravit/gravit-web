20a752a9b0ded692ad3121d4121baf3045a854cc
조하은 <jhe93@naver.com>
2026-06-04 21:28:07 +0900

    fix: use-unfollow, use-follow 훅 수정

--- src/features/follow/model/use-follow.ts
+++ src/features/follow/model/use-follow.ts
@@ -3,28 +3,34 @@ import { type InfiniteData, useQueryClient } from '@tanstack/react-query';
 import {
   getGetFollowAndFollowingCountQueryKey,
   getGetFollowersInfiniteQueryKey,
+  getGetFollowingsInfiniteQueryKey,
   useFollowing,
 } from '@/shared/api/@generated/friend-api/friend-api';
 import type {
   FollowCountsResponse,
-  SliceResponseFollowingResponse,
 } from '@/shared/api/@generated/model';
+import type { SliceResponseFollowerResponse } from '@/shared/api/@generated/model/sliceResponseFollowerResponse';
 
+// 팔로워 페이지에서 팔로우 요청을 보내는 훅
 export function useFollow({ onSuccess }: { onSuccess?: () => void } = {}) {
   const queryClient = useQueryClient();
 
   return useFollowing({
     mutation: {
       onMutate: async ({ followeeId }) => {
+
+        // 팔로워 목록 또는 팔로워 수 api 호출 중지
         await Promise.all([
           queryClient.cancelQueries({ queryKey: getGetFollowersInfiniteQueryKey() }),
           queryClient.cancelQueries({ queryKey: getGetFollowAndFollowingCountQueryKey() }),
         ]);
 
+        // 현재 팔로잉 목록/수 저장
         const previousFollower = queryClient.getQueryData(getGetFollowersInfiniteQueryKey());
         const previousCount = queryClient.getQueryData(getGetFollowAndFollowingCountQueryKey());
 
-        queryClient.setQueryData<InfiniteData<SliceResponseFollowingResponse>>(
+        // 현재 팔로잉 목록에서 isFollowing 값 수정
+        queryClient.setQueryData<InfiniteData<SliceResponseFollowerResponse>>(
           getGetFollowersInfiniteQueryKey(),
           (old) => {
             if (!old) return old;
@@ -32,12 +38,15 @@ export function useFollow({ onSuccess }: { onSuccess?: () => void } = {}) {
               ...old,
               pages: old.pages.map((page) => ({
                 ...page,
-                contents: page.contents?.filter((user) => user.id !== followeeId),
+                contents: page.contents?.map((user) =>
+                  user.id === followeeId ? { ...user, isFollowing: true } : user
+                ),
               })),
             };
           },
         );
 
+        // 팔로잉 수 1 증가
         queryClient.setQueryData<FollowCountsResponse>(
           getGetFollowAndFollowingCountQueryKey(),
           (old) => {
@@ -57,6 +66,7 @@ export function useFollow({ onSuccess }: { onSuccess?: () => void } = {}) {
       },
       onSettled: () => {
         queryClient.invalidateQueries({ queryKey: getGetFollowersInfiniteQueryKey() });
+        queryClient.invalidateQueries({ queryKey: getGetFollowingsInfiniteQueryKey() });
         queryClient.invalidateQueries({ queryKey: getGetFollowAndFollowingCountQueryKey() });
       },
     },
--- src/features/follow/model/use-unfollow.ts
+++ src/features/follow/model/use-unfollow.ts
@@ -2,6 +2,7 @@ import { type InfiniteData, useQueryClient } from '@tanstack/react-query';
 
 import {
   getGetFollowAndFollowingCountQueryKey,
+  getGetFollowersInfiniteQueryKey,
   getGetFollowingsInfiniteQueryKey,
   useUnFollowing,
 } from '@/shared/api/@generated/friend-api/friend-api';
@@ -9,6 +10,7 @@ import type {
   FollowCountsResponse,
   SliceResponseFollowingResponse,
 } from '@/shared/api/@generated/model';
+import type { SliceResponseFollowerResponse } from '@/shared/api/@generated/model/sliceResponseFollowerResponse';
 
 export function useUnfollow({ onSuccess }: { onSuccess?: () => void } = {}) {
   const queryClient = useQueryClient();
@@ -16,14 +18,20 @@ export function useUnfollow({ onSuccess }: { onSuccess?: () => void } = {}) {
   return useUnFollowing({
     mutation: {
       onMutate: async ({ followeeId }) => {
+
+        // 팔로잉 목록과 팔로워 목록, 팔로워 수 패칭 api를 중단
         await Promise.all([
           queryClient.cancelQueries({ queryKey: getGetFollowingsInfiniteQueryKey() }),
+          queryClient.cancelQueries({ queryKey: getGetFollowersInfiniteQueryKey() }),
           queryClient.cancelQueries({ queryKey: getGetFollowAndFollowingCountQueryKey() }),
         ]);
 
+        //  현재 팔로잉 수와 현재 팔로잉 목록/팔로워 목록 저장
         const previousFollowings = queryClient.getQueryData(getGetFollowingsInfiniteQueryKey());
+        const previousFollowers = queryClient.getQueryData(getGetFollowersInfiniteQueryKey());
         const previousCount = queryClient.getQueryData(getGetFollowAndFollowingCountQueryKey());
 
+        // 팔로잉 목록에서 언팔로우가 눌린 유저를 제거
         queryClient.setQueryData<InfiniteData<SliceResponseFollowingResponse>>(
           getGetFollowingsInfiniteQueryKey(),
           (old) => {
@@ -38,6 +46,22 @@ export function useUnfollow({ onSuccess }: { onSuccess?: () => void } = {}) {
           },
         );
 
+        // 팔로워 목록에서 언팔로우 누른 유저와의 관계 변경
+        queryClient.setQueryData<InfiniteData<SliceResponseFollowerResponse>>(
+          getGetFollowersInfiniteQueryKey(),
+          (old) => {
+            if (!old) return old;
+            return {
+              ...old,
+              pages: old.pages.map((page) => ({
+                ...page,
+                contents: page.contents?.map((user)=> user.id === followeeId ? {...user,isFollowing: false} : user),
+              })),
+            };
+          },
+        );
+
+        // 팔로잉 수를 1 감소
         queryClient.setQueryData<FollowCountsResponse>(
           getGetFollowAndFollowingCountQueryKey(),
           (old) => {
@@ -46,17 +70,19 @@ export function useUnfollow({ onSuccess }: { onSuccess?: () => void } = {}) {
           },
         );
 
-        return { previousFollowings, previousCount };
+        return { previousFollowings, previousFollowers, previousCount };
       },
       onSuccess: () => {
         onSuccess?.();
       },
       onError: (_, __, context) => {
         queryClient.setQueryData(getGetFollowingsInfiniteQueryKey(), context?.previousFollowings);
+        queryClient.setQueryData(getGetFollowersInfiniteQueryKey(), context?.previousFollowers);
         queryClient.setQueryData(getGetFollowAndFollowingCountQueryKey(), context?.previousCount);
       },
       onSettled: () => {
         queryClient.invalidateQueries({ queryKey: getGetFollowingsInfiniteQueryKey() });
+        queryClient.invalidateQueries({ queryKey: getGetFollowersInfiniteQueryKey() });
         queryClient.invalidateQueries({ queryKey: getGetFollowAndFollowingCountQueryKey() });
       },
     },
