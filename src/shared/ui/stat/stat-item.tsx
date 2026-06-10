7b1dd64119527910c964ee069fdf8c25f6052bb1
Ajungmiiin <wjdals3249@naver.com>
2026-06-11 02:09:47 +0900

    feat : className Props 추가

--- src/shared/ui/stat/stat-item.tsx
+++ src/shared/ui/stat/stat-item.tsx
@@ -5,16 +5,20 @@ interface StatItemProps {
   label: string;
   highlight?: boolean;
   onClick?: () => void;
+  className?: string;
 }
 
-function StatItem({ value, label, highlight, onClick }: StatItemProps) {
+function StatItem({ value, label, highlight, onClick, className }: StatItemProps) {
   const Component = onClick ? 'button' : 'div';
 
   return (
     <Component
       type={onClick ? 'button' : undefined}
       onClick={onClick}
-      className="flex items-center justify-center flex-col gap-1 md:bg-bg-2 rounded-xl  md:py-8 flex-1"
+      className={cn(
+        'flex items-center justify-center flex-col gap-1 md:bg-bg-2 rounded-xl  md:py-8 flex-1',
+        className,
+      )}
     >
       <p className={cn('text-headline2 md:text-title1', highlight ? 'text-main' : 'text-text-1')}>
         {value}
