7af159ebf53be08bf09f0c2c5866704eb52b245f
Ajungmiiin <wjdals3249@naver.com>
2026-07-30 20:03:51 +0900

    refactor : nickname-form, profile-selector 컴포넌트 리팩토링
    - onboarding -> profile 폴더로 이동
    - profile-selector 제어 컴포넌트로 수정
    - nickname-form helperText 컴포넌트로 내로 이동
    - isCompact Props를 통해 스타일 제어

--- src/features/profile/ui/profile-selector.tsx
+++ src/features/profile/ui/profile-selector.tsx
@@ -1,52 +1,51 @@
-import { useEffect, useState } from 'react';
-
 import LeftArrow from '@/shared/assets/icons/buttons/left-gray-arrow.svg?react';
 import RightArrow from '@/shared/assets/icons/buttons/right-gray-arrow.svg?react';
 import Profile from '@/shared/assets/icons/profile2.svg?react';
+import { cn } from '@/shared/lib/cn';
 import { PROFILE_COLORS } from '@/shared/lib/ProfileColor';
 
 interface ProfileSelectorProps {
-  value?: number;
-  onChange?: (index: number) => void;
-  gap?: number;
+  colorIndex: number;
+  onChange: (index: number) => void;
+  className?: string;
+  isCompact?: boolean;
 }
 
-export default function ProfileSelector({ value, onChange, gap = 10 }: ProfileSelectorProps) {
-  const [colorIndex, setColorIndex] = useState(value ?? 0);
-
+export default function ProfileSelector({
+  colorIndex,
+  onChange,
+  className,
+  isCompact,
+}: ProfileSelectorProps) {
   const colorKeys = Object.keys(PROFILE_COLORS).map(Number) as (keyof typeof PROFILE_COLORS)[];
-
-  useEffect(() => {
-    if (value !== undefined) {
-      setColorIndex(value);
-    }
-  }, [value]);
 
   const handlePrev = () => {
     const newIndex = colorIndex === 0 ? colorKeys.length - 1 : colorIndex - 1;
-    setColorIndex(newIndex);
-    onChange?.(newIndex);
+    onChange(newIndex);
   };
 
   const handleNext = () => {
     const newIndex = colorIndex === colorKeys.length - 1 ? 0 : colorIndex + 1;
-    setColorIndex(newIndex);
-    onChange?.(newIndex);
+    onChange(newIndex);
   };
 
   return (
-    <div className="flex flex-row items-center justify-center" style={{ gap: `${gap * 0.25}rem` }}>
+    <div className={cn('flex flex-row items-center justify-center', className)}>
       <button type="button" onClick={handlePrev}>
-        <LeftArrow className="mt-0.5 cursor-pointer text-text-3-w md:text-text-1-w" />
+        <LeftArrow
+          className={cn('mt-0.5 cursor-pointer text-text-3-w', !isCompact && 'md:text-text-1-w')}
+        />
       </button>
 
       <Profile
-        className="size-[150px] md:size-[178px]"
+        className={cn('size-[150px]', !isCompact && 'md:size-[178px]')}
         style={{ color: PROFILE_COLORS[colorKeys[colorIndex]] }}
       />
 
       <button type="button" onClick={handleNext}>
-        <RightArrow className="cursor-pointer text-text-3-w md:text-text-1-w" />
+        <RightArrow
+          className={cn('cursor-pointer text-text-3-w', !isCompact && 'md:text-text-1-w')}
+        />
       </button>
     </div>
   );
